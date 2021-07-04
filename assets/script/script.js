6b7fee5bebb3229b4648918e4c0a062d61217636
Nick Zabkar <nzabkar51@gmail.com>
2021-07-03 22:27:17 -0400

    Added and defined functions and parameters

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,6 +14,57 @@ let confirmNumericCharacter;
 let confirmUpperCase;
 let confirmLowerCase;
 
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to be?"));
+
+  while(confirmLength <= 7 || confirmLength >= 129) {
+      alert("Password length must be between 8-128 characters.");
+      var confirmLength = (prompt("How many characters would you like your password to be?"));
+      } 
+  
+      alert(`Your password will be ${confirmLength} characters long`);
+
+      var confirmLowerCase = confirm("Would you like to include lowercase characters?");
+      var confirmUpperCase = confirm("Would you like to include uppercase characters?");
+      var confirmNumericCharacter = confirm("Would you like to include numbers?");    
+      var confirmSpecialCharacter = confirm("Would you like to include special characters?");
+ 
+      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+        alert("Choose at least one");
+        var confirmLowerCase = confirm("Would you like to include lowercase characters?");
+        var confirmUpperCase = confirm("Would you like to include uppercase characters?");   
+        var confirmNumericCharacter = confirm("Would you like to include numeric characters?");    
+        var confirmSpecialCharacter = confirm("Would you like to include special characters?");
+    }
+
+      var passwordCharacters = []
+      
+    if (confirmSpecialCharacter) {
+      passwordCharacters = passwordCharacters.concat(specialChar)
+    }
+
+    if (confirmNumericCharacter) {
+      passwordCharacters = passwordCharacters.concat(number)
+    }
+      
+    if (confirmLowerCase) {
+      passwordCharacters = passwordCharacters.concat(alphaLower)
+    }
+
+    if (confirmUpperCase) {
+      passwordCharacters = passwordCharacters.concat(alphaUpper)
+    }
+
+      console.log(passwordCharacters)
+
+      var randomPassword = ""
+      
+      for (var i = 0; i < confirmLength; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+      }
+      return randomPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
